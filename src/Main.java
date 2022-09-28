20d9943cff5b0a823416d8b7c3f22ee9ad0087d2
Aleksey Esin <aesin87@mail.ru>
2022-09-28 12:29:37 +0300

    Added branch eraser

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.function.Predicate;
 
 public class Main {
     public static void main(String[] args) {
@@ -8,11 +9,15 @@ public class Main {
         persons.add(new Person("Антуан де", "Сент-Экзюпери", 44));
         persons.add(new Person("Гарриет", "Бичер-Стоу", 85));
         persons.add(new Person("Максим", "Горький", 68));
+        persons.add(new Person("Изабель", "Харрис", 14));
         persons.add(new Person("Дмитрий", "Мамин-Сибиряк", 60));
         persons.add(new Person("Габриэль", "Гарсиа Маркес", 87));
         persons.add(new Person("Шарль Мари Рене", "Леконт де Лиль", 75));
         persons.add(new Person("Михаил", "Булгаков", 48));
+        persons.add(new Person("Ритай", "Аль-Хазми", 14));
 
+        Predicate<Person> pr1 = age -> age.getAge() < 18;
+        persons.removeIf(pr1);
         persons.sort(new PersonsComparator(2));
         System.out.println(persons);
 
